0f92f2b83cd043ea3720277646287807eafda5a4
prajun <prajunbudathoki1@gmail.com>
2025-05-30 13:50:06 +0545

    added better auth

---
+++ app/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from "@/generated/prisma";
+import { admin } from "better-auth/plugins";
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [admin({ adminRoles: ["admin", "superadmin"] })],
+});
